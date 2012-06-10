560aabd083b1146090544070bb1fafcfc8076176
Benoît Garrigues <bgarrigues@gmail.com>
2012-06-10 19:44:42 +0200

    Fix log messages

--- testSQLite.m
+++ testSQLite.m
@@ -104,7 +104,7 @@ main()
 
   if ([records count] != 2)
     {
-      NSLog(@"Expected 2 records but got %u", [records count]);
+      NSLog(@"Expected 2 records but got %lu", [records count]);
     }
   else
     {
